595f126ac6a76cb4c606fb37624e32f99a9a2e02
Omprakash choudhari <choudharyomprakash147@gmail.com>
2020-05-26 09:52:54 +0530

    first_mail

---
+++ mes.py
@@ -0,0 +1,10 @@
+import smtplib as s
+op=s.SMTP("smtp.gmail.com",587)
+op.starttls()
+op.login("omprakash.21910935@viit.ac.in","Sukhiom@1710")
+subject="Good news"
+boby="Congratulation your model trigger more then 90% accuracy"
+message="Subject:{}\n\n{}".format(subject,boby)
+listOfAddress=["choudharyomprakash147@gmail.com"]
+op.sendmail("choudharyomprakash",listOfAddress,message)
+op.quit()
