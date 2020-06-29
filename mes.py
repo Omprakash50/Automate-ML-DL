6bfeea270a4adc3faad0cb3056407d19c18cf317
Omprakash choudhari <56760981+Omprakash50@users.noreply.github.com>
2020-06-29 08:09:34 +0530

    Update mes.py

--- mes.py
+++ mes.py
@@ -1,7 +1,7 @@
 import smtplib as s
 op=s.SMTP("smtp.gmail.com",587)
 op.starttls()
-op.login("omprakash.21910935@viit.ac.in","Sukhiom@1710")
+op.login("omprakash.21910935@viit.ac.in","**********")
 subject="Good news"
 boby="Congratulation your model trigger more then 90% accuracy"
 message="Subject:{}\n\n{}".format(subject,boby)
